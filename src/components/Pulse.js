49d9d740e6e2a3694da5485766f119afff617460
unknown <goranikukic@gmail.com>
2022-10-30 18:03:37 +0100

    Notification bell almost finished

---
+++ src/components/Pulse.js
@@ -0,0 +1,13 @@
+import pulseIcon from '../images/pulse.svg';
+import meetingIcon from '../images/meeting.svg';
+import itemsIcon from '../images/items.svg';
+import actionsIcon from '../images/actions.svg';
+
+const Pulse = () => {
+  return (
+    <div className="pulse">
+    </div>
+  );
+};
+
+export default Pulse;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Effect } from 'react-notification-badge';
 import NotificationBadge from 'react-notification-badge/lib/components/NotificationBadge';
 import logoIcon from '../images/logo.svg';
@@ -6,8 +6,24 @@ import notificationIcon from '../images/notification.svg';
 
 const TopBar = () => {
   const [notifBadgeIsShown, setNotifBadgeShown] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick, true);
+  });
+
+  const refOne = useRef(null);
+
+  const handleClick = (e) => {
+    if (!refOne.current.contains(e.target)) {
+      setNotifBadgeShown(false);
+    }
+    if (refOne.current.contains(e.target)) {
+      setNotifBadgeShown(false);
+    }
+  };
+
   const notifications = ['Notification 1', 'Notification 2'];
-  
+
   let listItems = notifications.map((number) => (
     <li key={number.toString()}>{number}</li>
   ));
@@ -17,7 +33,11 @@ const TopBar = () => {
       <img className="logo-icon" src={logoIcon} alt="Logo icon" />
       <div
         className="notification-wrapper"
-        onClick={() => setNotifBadgeShown(true)}
+        onClick={() =>
+          notifBadgeIsShown === false
+            ? setNotifBadgeShown(true)
+            : setNotifBadgeShown(false)
+        }
       >
         {!notifBadgeIsShown ? (
           <NotificationBadge
@@ -32,6 +52,7 @@ const TopBar = () => {
           className="notification-icon"
           src={notificationIcon}
           alt="Notification icon"
+          ref={refOne}
         />
       </div>
     </div>
